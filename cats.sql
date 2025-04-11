bd4e51de90c4f2889aed413586d67bcac3318e5b
Hung Nguyen <nguyenlehungst@gmail.com>
2025-04-11 16:34:31 -0700

    MVC finished add demo sql

--- cats.sql
+++ cats.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Apr 11, 2025 at 02:53 AM
--- Server version: 8.0.39
--- PHP Version: 8.2.27
+-- Generation Time: Apr 11, 2025 at 11:31 PM
+-- Server version: 8.0.41
+-- PHP Version: 8.2.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -30,10 +30,10 @@ SET time_zone = "+00:00";
 CREATE TABLE `cats` (
   `cat_id` int NOT NULL,
   `age` double NOT NULL,
-  `breed` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `description` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `name` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `img_path` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL
+  `breed` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `description` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `img_path` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -41,9 +41,12 @@ CREATE TABLE `cats` (
 --
 
 INSERT INTO `cats` (`cat_id`, `age`, `breed`, `description`, `name`, `img_path`) VALUES
-(1, 1, 'Abyssianian', 'The Abyssinian is easy to care for, and a joy to have in your home. They’re affectionate cats and love both people and other animals.', 'Hachimaru', '/images/1.jpg'),
-(2, 2, 'American Bobtail', 'American Bobtails are known for their stubby, bobbed tails, dog-like personalities, and friendly, adaptable temperaments, often bonding deeply with their human families.', 'Chuchunmaru', '/images/2.jpg'),
-(3, 3, 'Chartreux', 'Lazy and like to eat', 'Yomusuke', NULL);
+(1, 1, 'Abyssianian', 'The Abyssinian is easy to care for, and a joy to have in your home. They’re affectionate cats and love both people and other animals.', 'Hachimaru', '/images/2.jpg'),
+(2, 3, 'American Bobtail', 'American Bobtails are known for their stubby, bobbed tails, dog-like personalities, and friendly, adaptable temperaments, often bonding deeply with their human families.', 'Chuchunmaru', '/images/1.jpg'),
+(3, 3, 'Chartreux', 'Lazy and like to eat', 'Yomusuke', NULL),
+(5, 1.5, 'tabby', 'Hangry', 'test cat', '/images/4.jpg'),
+(6, 2.3, 'British Short Hair', 'Squishy', 'Another Cat', '/images/6.jpg'),
+(7, 0.8, 'Siamese', 'White', 'And Another One', '/images/7.jpg');
 
 --
 -- Indexes for dumped tables
@@ -63,7 +66,7 @@ ALTER TABLE `cats`
 -- AUTO_INCREMENT for table `cats`
 --
 ALTER TABLE `cats`
-  MODIFY `cat_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `cat_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
